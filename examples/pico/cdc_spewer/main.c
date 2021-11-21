2e935582791056a021a97247c4b3f194dd08a0bf
OXullo Intersecans <x@xul.it>
2021-11-21 17:25:59 +0100

    Blink the onboard led to signal activity

--- examples/pico/cdc_spewer/main.c
+++ examples/pico/cdc_spewer/main.c
@@ -3,14 +3,20 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
+const uint LED_PIN = 25;
 
 int main()
 {
-    printf("sin\n");
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
     stdio_init_all();
     double alpha = 0;
 
+    printf("sin\n");
+
     while (true) {
+        gpio_put(LED_PIN, !gpio_get(LED_PIN));
         printf("%f\n", sin(alpha));
         alpha += 0.2;
         sleep_ms(30);
