fda5898d7e25f861b63b8b95d4c7e8a793589dc1
OXullo Intersecans <x@xul.it>
2021-11-22 22:53:11 +0100

    Gradually increment the amplitude

--- examples/pico/cdc_spewer/main.c
+++ examples/pico/cdc_spewer/main.c
@@ -4,6 +4,7 @@
 #include "hardware/gpio.h"
 
 const uint LED_PIN = 25;
+uint iterations = 0;
 
 int main()
 {
@@ -12,13 +13,18 @@ int main()
 
     stdio_init_all();
     double alpha = 0;
+    double amplitude = 1;
 
     printf("sin\n");
 
-    while (true) {
+    while (1) {
+        ++iterations;
         gpio_put(LED_PIN, !gpio_get(LED_PIN));
-        printf("%f\n", sin(alpha));
+        printf("%f\n", amplitude * sin(alpha));
         alpha += 0.2;
+        if (iterations % 1000 == 0) {
+            amplitude += 0.5;
+        }
         sleep_ms(30);
     }
 
